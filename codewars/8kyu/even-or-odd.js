052ca882cbd91531e7bff90bbe2bebb14b5d429f
Justinmemphis <jcarter82@gmail.com>
2023-02-06 14:27:06 -0600

    daily coding challenge

---
+++ codewars/8kyu/even-or-odd.js
@@ -0,0 +1,21 @@
+/*
+Even Or Odd
+8kyu
+
+Description:
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+Mathematics
+Fundamentals
+
+*/
+
+// Solution:
+
+function evenOrOdd(number) {
+     if ( number % 2 == 0) {
+	return "Even";
+     } else {
+     	return "Odd";
+     }
+}
