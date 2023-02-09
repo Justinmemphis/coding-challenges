b5d05c405296e2cade0c6afa7928f77f5660588e
Justinmemphis <jcarter82@gmail.com>
2023-02-09 14:19:48 -0600

    daily coding challenge

---
+++ codewars/8kyu/sum-of-positive.js
@@ -0,0 +1,24 @@
+/*
+ Sum of Positive
+ 8kyu
+
+ Description:
+ You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+Arrays
+Fundamentals
+*/
+
+// Solution:
+function positiveSum(arr) {
+  var newSum = 0;
+  for (var i = 0; i<arr.length;i++){
+    if (arr[i]>0) {
+      newSum += arr[i];
+    }
+  }
+  return newSum;
+}
