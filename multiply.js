99c0d3c328bb159327af65159201c404615b9c03
Justinmemphis <jcarter82@gmail.com>
2023-03-29 09:54:32 -0500

    finish multiplication function

---
+++ multiply.js
@@ -0,0 +1,27 @@
+/* Multiply
+ * 8kyu
+ *
+ * Description:
+ *
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a,b) {
+	a * b
+}
+
+Debugging
+Fundamentals
+*/
+
+// Solution
+// PREP - Parameters, Results, Examples, Pseudocode
+// Parameters - receive a function with two parameters
+// Results - return the multiplication of the two parameters
+// Example - (2,2) -> 4
+// Pseudocode:
+// 	return the multiple of a * b
+
+function multiply(a,b) {
+	return a*b;
+}
