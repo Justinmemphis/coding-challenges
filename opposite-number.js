e48799de2a88881f07f3fc0e95ad24cb9ae67c14
Justinmemphis <jcarter82@gmail.com>
2023-03-30 10:51:25 -0500

    return opposite of a number

---
+++ opposite-number.js
@@ -0,0 +1,29 @@
+/* Opposite Number
+ * 8kyu
+ *
+ Description:
+
+
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Examples, Pseudocode
+// Parameters - receive an integer or floating point number
+// Results - return the oppositive of the number
+// Example - (4) -> -4
+// Pseudocode:
+// 	return the opposite of the number
+
+function opposite(number) {
+	return -number;
+}
