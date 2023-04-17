71063f2f408bb34efe2e91c63f4aa35559054e9b
Justinmemphis <jcarter82@gmail.com>
2023-04-16 19:45:16 -0500

    like fibonacci sequence but with three numbers

---
+++ tribonacci-sequence.js
@@ -0,0 +1,61 @@
+/* Tribonacci Sequence
+ * 6kyu
+ *
+ * Description:
+ 
+
+Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+
+But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+
+Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+
+[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+Number Theory
+Arrays
+Lists
+Fundamentals
+*/
+
+// PREP - Parameters, Results, Examples, Pseudocode
+// Parameters - receive three numbers in an array in ascending order (non-negative numbers only); also receive a value n that is 0 or greater
+// Results - if n is zero, return an empty array; otherwise return the first n values of the sequence starting from the first three (and adding three numbers together)
+// Examples - see above
+// Pseudocode:
+// 	if n is zero return empty array
+// 	else
+// 		create result array
+// 		iterate up to n
+// 			add numbers to result array
+// 		return result array
+
+function tribonacci(signature,n){
+	         if (n == [] || 0) {
+                return [];
+        } else if (n < 3) {
+                let result = [];
+                for (var i = 0; i < n; i++) {
+                  result.push(signature[i]);
+                };
+                return result;
+        } else {
+                let result = signature;
+                for (var i = 3; i < n; i++) {
+                        let temp = result[i-1] + result[i-2] + result[i-3];
+                        result.push(temp);
+                };
+                return result;
+        };
+}
