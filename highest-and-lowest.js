8c5a2af3de18fe9774fdb9df73438811ec5d3ec6
Justinmemphis <jcarter82@gmail.com>
2023-05-13 16:57:33 -0500

    find highest and lowest number in a string of digits

---
+++ highest-and-lowest.js
@@ -0,0 +1,44 @@
+/* Highest and Lowest
+ * 7kyu
+ *
+ * Description:
+ 
+
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+
+Fundamentals
+Strings
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a string of numbers
+// Results - return the highest and lowest numbers of the string
+// Example - see above
+// Pseudocode:
+// 	create new array of string separated by spaces
+// 	return max number then min number
+
+function highAndLow(numbers){
+	          let result = numbers.split(' ').map(Number);
+        let max = Math.max(...result);
+        let min = Math.min(...result);
+        return `${max} ${min}`;
+}
+
+
+
+
+
