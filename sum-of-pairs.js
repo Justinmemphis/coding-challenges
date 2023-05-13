879a62cc091789fbb7411ab0a7ddba7012a9ac08
Justinmemphis <jcarter82@gmail.com>
2023-05-13 16:49:11 -0500

    two sum problem in javascript

---
+++ sum-of-pairs.js
@@ -0,0 +1,62 @@
+/* Sum of Pairs
+ * 5kyu
+ *
+ * Description:
+ Sum of Pairs
+
+Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+If there are two or more pairs with the required sum, the pair whose second element has the smallest index is the solution.
+
+sum_pairs([11, 3, 7, 5],         10)
+#              ^--^      3 + 7 = 10
+== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],         6)
+#          ^-----^         4 + 2 = 6, indices: 0, 2 *
+#             ^-----^      3 + 3 = 6, indices: 1, 3
+#                ^-----^   2 + 4 = 6, indices: 2, 4
+#  * the correct answer is the pair whose second value has the smallest index
+== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)
+#  there are no pairs of values that can be added to produce 2.
+== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#              ^-----------^   5 + 5 = 10, indices: 1, 5
+#                    ^--^      3 + 7 = 10, indices: 3, 4 *
+#  * the correct answer is the pair whose second value has the smallest index
+== [3, 7]
+
+Negative numbers and duplicate numbers can and will appear.
+
+NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+Memoization
+Fundamentals
+Performance
+*/
+
+// Solution:
+// PREP - Parameters, Results, Examples, Pseudocode
+// Parameters - receive an array of integers and a sum value
+// Results - return where two consecutive integers equals the sum value; if duplicate return first pair
+// Example - see above
+// Pseudocode:
+// 	create result variable
+// 	iterate over the list
+// 		iterate over the list again
+// 			if first iterate + second iterate equals sum
+// 				result = pair
+// 	return result variable
+// 	
+// 	Note - didn't solve but came close - see answer below for what other people did
+
+var sum_pairs=function(ints, s){
+  var seen = {}
+  for (var i = 0; i < ints.length; ++i) {
+    if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+    seen[ints[i]] = true
+  }
+}
+
