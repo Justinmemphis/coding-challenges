a13c9743170d78d77637350eda5ee091ace054b1
Justinmemphis <jcarter82@gmail.com>
2023-05-19 21:36:00 -0500

    filter a list for numbers only

---
+++ list-filtering.js
@@ -0,0 +1,34 @@
+/* List Filtering
+ * 7kyu
+ *
+ * Description:
+ 
+
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+Example
+
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+Lists
+Filtering
+Data Structures
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Examples, Pseudocode
+// Parameters - receive an array of non-negative integers and strings
+// Results - return only the integers (no strings)
+// Example - see above
+// Pseudocode:
+// 	return array filtered by numbers only
+
+function filter_list(l) {
+        function testIfNumber(x) {
+                return (typeof x) == 'number';
+        };
+
+        return l.filter(testIfNumber);
+}
