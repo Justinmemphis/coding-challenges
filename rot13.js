6cfff9521a75e5b958225f2f93a6b2fc975da82b
Justinmemphis <jcarter82@gmail.com>
2023-06-04 19:44:48 -0500

    decrypt rot13 cipher

--- rot13.js
+++ rot13.js
@@ -66,7 +66,7 @@ function rot13(message) {
   var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
   return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
-}
+/}
 
 
 
