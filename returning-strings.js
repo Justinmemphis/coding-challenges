66a04bb53dbbe75b4d34d2574ae7ce6093b16c44
Justinmemphis <jcarter82@gmail.com>
2023-06-20 11:08:23 -0500

    return a given string inside another string

---
+++ returning-strings.js
@@ -0,0 +1,25 @@
+/* Returning Strings
+ * 8kyu
+ *
+ * Description:
+ 
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+Strings
+Fundamentals
+
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a string
+// Result - return the string inside another string
+// Example - see above
+// Pseudocode
+// 	use template literals
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}i
