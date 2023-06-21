e542949584149356e13828436094d2922c549f1f
Justinmemphis <jcarter82@gmail.com>
2023-06-21 15:04:21 -0500

    return hello world

---
+++ function-1-hello-world.js
@@ -0,0 +1,24 @@
+/* Function 1 - hello world
+ * 8kyu
+ *
+ * Description:
+ 
+
+Make a simple function called greet that returns the most-famous "hello world!".
+Style Points
+
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - none
+// Results - return 'hello world' when the function is run
+// Example - () -> 'hello world'
+// Pseudocode:
+// 	return 'hello world';
+
+function greet() {
+	return 'hello world!';
+}
