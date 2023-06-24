4465e6b016ac3fcc9dada9f50b1e66f98a2ade6d
Justinmemphis <jcarter82@gmail.com>
2023-06-23 22:13:43 -0500

    convert boolean to a string

---
+++ convert-a-boolean-to-a-string.js
@@ -0,0 +1,24 @@
+/* Convert a Boolean to a String
+ * 8kyu
+ *
+ * Description:
+ 
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+Strings
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a boolean true or false
+// Results - return the boolean as a string
+// Example - true -> 'true'
+// Pseudocode:
+// 	boolean .toString()
+
+function booleanToString(b){
+	return b.toString();
+}
