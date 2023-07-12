a7a19360d9309b70405f1aa5003b4ded7760c830
Justinmemphis <jcarter82@gmail.com>
2023-07-12 18:36:59 -0500

    make string uppercase

---
+++ makeuppercase.js
@@ -0,0 +1,22 @@
+/* MakeUpperCase
+ * 8kyu
+ *
+ * Description:
+ 
+
+Write a function which converts the input string to uppercase.
+Fundamentals
+Strings
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a string
+// Results - return the same string in uppercase
+// Example - hello -> HELLO
+// Pseudocode:
+// 	return string to upper case
+
+function makeUpperCase(str) {
+	return str.toUpperCase();
+}
