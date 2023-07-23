efe71727c8329a4abf6e74bcea92bdfba2aff13b
Justinmemphis <jcarter82@gmail.com>
2023-07-23 09:11:13 -0500

    double an integer

---
+++ you-cant-code-under-pressure.js
@@ -0,0 +1,14 @@
+/* You Can't Code Under Pressure #1
+ * 8kyu
+ *
+ Description:
+
+
+Code as fast as you can! You need to double the integer and return it.
+Fundamentals
+*/
+
+// My solution:
+function doubleInteger(i) {
+  return i * 2;
+}
