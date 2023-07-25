e079d2cf70f503e4447d7fda7d7951690576b543
Justinmemphis <jcarter82@gmail.com>
2023-07-25 06:25:02 -0500

    get the mean of an array using reduce

---
+++ get-the-mean-of-an-array.js
@@ -0,0 +1,20 @@
+/* Get the mean of an array
+ * 8kyu
+ *
+ * Description:
+ 
+
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+Arrays
+Algorithms
+*/
+
+// my solution:
+
+function getAverage(marks){
+  return Math.floor((marks.reduce((sum,a) => sum + a, 0))/marks.length);
+}
