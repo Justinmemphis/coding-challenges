43b411a62922ac0c7a1cb3dd5311602c448aeaca
Justinmemphis <jcarter82@gmail.com>
2023-08-19 09:39:02 -0500

    convert a string to an integer

---
+++ parse-nice-int-from-char-problem.js
@@ -0,0 +1,23 @@
+/* Parse nice int from char problem
+ * 8kyu
+ *
+ * Description:
+ 
+
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a string between 0 and 9
+// Results - return the string as a number
+// use +inputString[0]
+
+function getAge(inputString){
+	return +inputString[0];
+};
