f3076d39aed113cb5b91260345b6f4aa03a5f754
Justinmemphis <jcarter82@gmail.com>
2023-09-03 10:10:00 -0500

    find difference in volume between two cuboids

---
+++ difference-of-volumes-of-cuboids.js
@@ -0,0 +1,27 @@
+/* Difference of Volumes of Cuboids
+ * 8kyu
+ *
+ * Description:
+ 
+
+In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.
+
+If you can, try writing it in one line of code.
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive two lists of integers of three positive integers each (cuboids)
+// Results - return the difference of volume between the two cuboids
+// Example - 2,2,3 and 5,4,1 -> 8
+// Pseudocode:
+// 	return 1 * 2 * 3 - 4 * 5 * 6
+
+function findDifference(a, b) {
+	return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]));
+};
