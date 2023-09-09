4ec786f40fc226f42df008ffdfd2290d425bb472
Justinmemphis <jcarter82@gmail.com>
2023-09-09 07:31:56 -0500

    square the number given

---
+++ function-2-squaring-an-argument.cs
@@ -0,0 +1,25 @@
+/* Function 2 - squaring an argument
+ * 8kyu
+ *
+ * Description:
+ 
+
+Now you have to write a function that takes an argument and returns the square of it.
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a double
+// Results - square the double and return a double
+// Example - 2 -> 4
+// Pseudocode
+// 	return the number squared
+
+public class Kata
+{
+	public static double Square(double x)
+	{
+		return x * x;
+	}
+}
