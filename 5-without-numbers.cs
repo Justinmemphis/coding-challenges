cdb9f25b8576996d5b018e9d9943f72462d2bd13
Justinmemphis <jcarter82@gmail.com>
2023-09-18 16:43:03 -0500

    return the number 5 without using digits or operators

---
+++ 5-without-numbers.cs
@@ -0,0 +1,39 @@
+/* 5 without numbers !!
+ * 8kyu
+ *
+ * Description:
+ 
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+Restricted
+Fundamentals
+Puzzles
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive nothing
+// Results - return the number five (without using numbers)
+// Example - return 5
+// Pseudocode:
+// 	return the unicode character at 5
+//
+// 	didn't solve - see favorite solution below
+
+class Kata
+{
+    public static int UnusualFive()
+    {
+        return "abcde".length;
+    }
+}
+
+
+
+
+
+
