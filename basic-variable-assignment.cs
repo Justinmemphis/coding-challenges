21da69cbf2603fdea7f4310c68105c3e9e0b360d
Justinmemphis <jcarter82@gmail.com>
2023-09-26 21:23:44 -0500

    assign string value

---
+++ basic-variable-assignment.cs
@@ -0,0 +1,24 @@
+/* Basic variable assignment
+ * 8kyu
+ *
+ * Description:
+ 
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+Debugging
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive nothing
+// Results - return "codewa.rs"
+// Example - n/a
+// Pseudocode:
+// 	return "codewa.rs"
+
+public class Kata
+{
+  public static string A = "code";
+  public static string B = "wa.rs";
+  public static string Name = A + B;
+}
