ff018237e6a6ba26bee6d747efa9ae16f5ac8112
Justinmemphis <jcarter82@gmail.com>
2023-10-28 21:10:49 -0500

    debug basic syntax of function call

---
+++ grasshopper-function-syntax-debugging.js
@@ -0,0 +1,16 @@
+/* Grasshopper - Function syntax debugging
+ * 8kyu
+ *
+ * Description:
+ 
+Grasshopper - Function syntax debugging
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// n/a - simple debugging exercise
+
+const main = (verb, noun) => verb + noun;
