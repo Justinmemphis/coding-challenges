d570ed7fff7607bb15221a778f5d023d489cb4ac
Justinmemphis <jcarter82@gmail.com>
2023-11-03 20:30:26 -0500

    remove digits from given string

---
+++ string-cleaning.cs
@@ -0,0 +1,36 @@
+/* String cleaning
+ * 8kyu
+ *
+ * Description:
+ 
+
+Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+Examples (input -> output)
+
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+Regular Expressions
+Fundamentals
+Strings
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a string
+// Results - return the string but with all digits 0-9 removed
+// Example - '123' -> ''
+// Pseudocode:
+// 	in string replace digit with ''
+
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static string StringClean(string s)
+  {
+    return Regex.Replace(s, "[0-9]", "");
+  }
+}
