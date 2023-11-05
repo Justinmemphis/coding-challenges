b9d70c93c3bb32930843d83f1b8e9a347f63ebe6
Justinmemphis <jcarter82@gmail.com>
2023-11-04 20:40:47 -0500

    return derivative of number and exponent

---
+++ take-the-derivative.cs
@@ -0,0 +1,42 @@
+/* Take the Derivative
+ * 8kyu
+ *
+ * Description:
+ 
+
+This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+For example:
+
+derive(7, 8)
+
+In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+derive(7, 8) --> this should output "56x^7" 
+derive(5, 9) --> this should output "45x^8" 
+
+Notes:
+
+    The output of this function should be a string
+    The exponent will never be 1, and neither number will ever be 0
+
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive two integers
+// Results - return a string of numbers multipled and exponent minus 1, with x in-between
+// Example - see above
+// Pseudocode: literal (a * b) x (b - 1)
+
+public class Kata
+{
+  public static string Derive(double coefficient, double exponent)
+  {
+	  return $"{coefficient * exponent}x^{exponent-1}";
+    
+  }
+}
