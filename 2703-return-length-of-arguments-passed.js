a824a390a5fb6a25d2c800b172f5a641f4093898
Justinmemphis <jcarter82@gmail.com>
2023-11-07 21:41:53 -0600

    return length of array

---
+++ 2703-return-length-of-arguments-passed.js
@@ -0,0 +1,64 @@
+/* 2703. Return Length of Arguments Passed
+ * LeetCode
+ * Easy
+ *
+ * Description:
+ 2703. Return Length of Arguments Passed
+Easy
+Companies
+Write a function argumentsLength that returns the count of arguments passed to it.
+
+ 
+
+Example 1:
+
+Input: args = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+
+One value was passed to the function so it should return 1.
+
+Example 2:
+
+Input: args = [{}, null, "3"]
+Output: 3
+Explanation: 
+argumentsLength({}, null, "3"); // 3
+
+Three values were passed to the function so it should return 3.
+
+ 
+
+Constraints:
+
+    args is a valid JSON array
+    0 <= args.length <= 100
+
+Accepted
+53.6K
+Submissions
+57.8K
+Acceptance Rate
+92.7%
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive an array
+// Results - return the length of the array
+// Example - see above
+// Pseudocode:
+// 	return length of array
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+	return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
