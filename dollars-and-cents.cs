11e00f513dca973f0d800ffb29228c16a1f00ea4
Justinmemphis <jcarter82@gmail.com>
2023-11-09 22:20:19 -0600

    return number in currency format

---
+++ dollars-and-cents.cs
@@ -0,0 +1,43 @@
+/* Dollars and Cents
+ * Codewars
+ * 8kyu
+ *
+ * Description:
+ 
+
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+39.99 becomes $39.99
+
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+Examples:
+
+3 needs to become $3.00
+
+3.1 needs to become $3.10
+
+Good luck! Your team knows they can count on you!
+Functional Programming
+Strings
+Algorithms
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive a double
+// Results - return the double formatted as currency
+// Example - 39.99 -> $39.99
+// Pseudocode:
+// 	double cents = amount % 1
+// 	if (cents length 
+//	double number = Math.floor(amount)
+//	return 
+
+public class Kata
+{
+  public static string FormatMoney(double amount)
+  {
+    return amount.ToString("$0.00");
+  }
+}
