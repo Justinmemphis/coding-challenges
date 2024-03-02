325daa8a1c429145b996718f2989c72c93dd44c8
Justinmemphis <jcarter82@gmail.com>
2024-03-01 20:44:57 -0600

    add a string to an array integer times

---
+++ kata-example-twist.py
@@ -0,0 +1,19 @@
+'''
+Kata Example Twist
+Codewars
+8kyu
+
+Description:
+
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+Fundamentals
+'''
+
+# Solution:
+# PREP - Parameters, Results, Example, Pseudocode
+# easy - just codewars * 1000
+
+websites = ['codewars'] * 1000
