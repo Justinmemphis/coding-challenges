c0e6e62c6776d68cd813ac0d658da66569e0681c
Justinmemphis <jcarter82@gmail.com>
2024-03-19 21:03:29 -0500

    divide one number by another number

---
+++ incorrect-division-method.py
@@ -0,0 +1,18 @@
+'''
+Incorrect division method
+Codewars
+8kyu
+
+Description:
+
+
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+Debugging
+'''
+
+# Solution:
+# PREP - Parameters, Results, Example, Pseudocode
+# n/a - just a debug exercise
+
+def divide_numbers(x,y):
+    return x / y
