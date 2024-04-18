7dbbb0136608f220e4875e5f61144d0637822b18
Justinmemphis <jcarter82@gmail.com>
2024-04-17 21:10:28 -0500

    find type of sum of two variables

---
+++ type-of-sum.js
@@ -0,0 +1,22 @@
+/* Type of sum
+ * Codewars
+ * 8kyu
+ *
+ * Description:
+ *
+
+Return the type of the sum of the two arguments
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// Parameters - receive two variables
+// Results - return the type of the sum of both variables
+// Example - n/a
+// Pseudocode:
+// 	type of (a + b)
+
+function typeOfSum(a, b) {
+  return typeof(a + b)
+}
