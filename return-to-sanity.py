7a1828f3a679878dc02bf4560e9d346513331ed3
Justinmemphis <jcarter82@gmail.com>
2024-04-18 22:27:52 -0500

    debug simple function

---
+++ return-to-sanity.py
@@ -0,0 +1,20 @@
+'''
+Return to Sanity
+Codewars
+8kyu
+
+Description:
+
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+Debugging
+'''
+
+# Solution:
+# PREP - Parameters, Results, Example, Pseudocode
+# n/a all - just a debug exercise
+
+def mystery():
+    results = {
+    'sanity': 'Hello'}
+    return results
