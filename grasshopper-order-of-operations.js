e9e3f7ffbd5af2a1273a2b7ae8d3fd4e9f9c4ff1
Justinmemphis <jcarter82@gmail.com>
2024-05-15 21:28:42 -0500

    fix order of operations

---
+++ grasshopper-order-of-operations.js
@@ -0,0 +1,19 @@
+/* Grasshopper - Order of operations
+ * Codewars
+ * 8kyu
+ *
+ * Description:
+ *
+Grasshopper Order of Operations
+
+You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+Fundamentals
+*/
+
+// Solution:
+// PREP - Parameters, Results, Example, Pseudocode
+// all n/a - this is just troubleshooting / debugging
+
+function orderOperations () {
+  return (2 + 2) * (2 + 2) * 2
+}
